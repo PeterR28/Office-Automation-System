f3cc9b36e85bf9da14b1571e1ea0812e4d62f3ed
PeterR28 <117482283+PeterR28@users.noreply.github.com>
2024-04-09 16:04:04 -0700

    Update SignInHandler.java

--- SignInHandler.java
+++ SignInHandler.java
@@ -1,1 +1,24 @@
 
+/*
+ * Author: Peter Rooney 
+ * Date: 03/04/24
+ * Description: This class deals with users signing in. First, it checks the account type. Next, 
+ * it checks if the username and password match any stored in the Office, and if it doesn’t 
+ * find any, it throws an error.
+ */
+public class SignInHandler 
+{
+
+		public Actor.Role checkAccountType(String username, String password) {
+	 
+			 Actor.Role accountType = Office.getInstance().checkAccountType(username, password);
+			 return accountType;
+			
+	        //if no user name or password then return nulll 
+	    }
+
+	    public void incorrectLoginError()
+	    {
+	    	System.out.println("wrong user name and password");
+	    }  
+}
