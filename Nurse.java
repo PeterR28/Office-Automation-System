b63db7e970cde78624f176f4f0f2e3915b8438df
General-I-M-Trash <164975483+General-I-M-Trash@users.noreply.github.com>
2024-04-09 20:58:29 -0700

    Update Nurse.java

--- Nurse.java
+++ Nurse.java
@@ -1,1 +1,20 @@
-
+public class Nurse extends Actor
+{
+	public Nurse()
+	{
+		this.role = Role.NURSE;
+	}
+	
+	public Nurse(String user, String pass)
+	{
+		this.role = Role.NURSE;
+		
+		this.username = user;
+		this.password = pass;
+	}
+	
+	public void saveVitals(String PatientId, String vitals)
+	{
+		
+	}
+}
