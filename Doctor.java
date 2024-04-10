709e7ce5b47fa414a7c9dc77fa69afbef0f876ab
General-I-M-Trash <164975483+General-I-M-Trash@users.noreply.github.com>
2024-04-09 20:58:03 -0700

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,1 +1,26 @@
+public class Doctor extends Actor
+{
+	public Doctor()
+	{
+		this.role = Role.DOCTOR;
+	}
+	
+	public Doctor(String user, String pass)
+	{
+		this.role = Role.DOCTOR;
+		
+		this.username = user;
+		this.password = pass;
+	}
+	
+	public String createNote()
+	{
+		return "Hello World";
+	}
+	
+	public void saveNote(String note)
+	{
+		
+	}
 
+}
